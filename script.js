f4ec9fe0838e4255816afab0529ec79fd5c7a4a8
aalmeida-mass <aalmeida@masslottery.com>
2024-07-30 10:11:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -323,12 +323,12 @@ restartButton.addEventListener('click', () => {
         .catch(error => console.error('Error fetching questions:', error));
 });
 
-// fetch('/questions.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         questions = selectRandomQuestions(data, 5);
-//         startGame();
-//     })
-//     .catch(error => console.error('Error fetching questions:', error));
+fetch('/questions.json')
+    .then(response => response.json())
+    .then(data => {
+        questions = selectRandomQuestions(data, 5);
+        startGame();
+    })
+    .catch(error => console.error('Error fetching questions:', error));
 
 nextButton.addEventListener('click', handleNextQuestion);
